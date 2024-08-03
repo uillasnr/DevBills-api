656e9b8038b90bd7b183af978434e9acff98f6ee
uillasnr <wiliasreis@hotmail.com>
2024-08-03 18:49:29 -0300

    fix: Middleware correction

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,12 +1,10 @@
-
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Response } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { StatusCodes } from "http-status-codes";
 import { AuthenticatedRequest } from "../Controllers/types";
 
-
 export default function authMiddleware(
-  req: AuthenticatedRequest<unknown>,
+  req: AuthenticatedRequest,
   res: Response,
   next: NextFunction
 ) {
@@ -19,12 +17,17 @@ export default function authMiddleware(
   const token = authorization.replace("Bearer ", "");
 
   try {
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET || "" ) as JwtPayload;
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET || "") as JwtPayload;
+
+    // Verifique se o token contém o userId
+    if (!decodedToken.userId) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({ error: "Unauthorized" });
+    }
 
     // Adiciona o usuário decodificado ao objeto de solicitação
-    req.user = { id: decodedToken.userId };
+    req.user = { id: decodedToken.userId }; // Agora, req.user está sempre definido
     return next();
-  } catch {
+  } catch (error) {
     return res.status(StatusCodes.UNAUTHORIZED).json({ error: "Unauthorized" });
   }
 }
