c71ec7e00a4a32e287db87fb7bcc1a086d2ff7d2
uillasnr <wiliasreis@hotmail.com>
2024-08-04 17:02:46 -0300

    fix: typo correction

--- src/Controllers/types.ts
+++ src/Controllers/types.ts
@@ -1,10 +1,24 @@
 import { Request } from "express";
 import { JwtPayload } from "jsonwebtoken";
 
-export interface AuthenticatedRequest<T> extends Request {
-  user: { id: string, name: string, email: string } | JwtPayload;
+// Interface para o usuário decodificado a partir do token JWT
+interface User {
+  id: string;
+  name: string;
+  email: string;
 }
 
+// Tipo que pode ser um objeto User ou JwtPayload
+export type UserOrJwtPayload = User | JwtPayload;
+
+// Interface para a solicitação autenticada que inclui o usuário
+export interface AuthenticatedRequest<T = any> extends Request {
+  user?: UserOrJwtPayload;
+  body: T;
+}
+
+// Tipo para solicitações que contêm um corpo (body)
 export type BodyRequest<T> = AuthenticatedRequest<T>;
-//export type QueryRequest<T> = Request<unknown, unknown, unknown, T> & { user: string | JwtPayload };
-export type QueryRequest<T> = Request<unknown, unknown, unknown, T> & { user: { id: string } };
+
+// Tipo para solicitações que contêm parâmetros de consulta (query)
+export type QueryRequest<T> = Request<unknown, unknown, unknown, T> & { user?: UserOrJwtPayload };
