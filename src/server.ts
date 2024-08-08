8869b3e96a32135644f506b3d839ff5669f04cfc
uillasnr <wiliasreis@hotmail.com>
2024-08-08 19:47:44 -0300

    fix: server

--- src/server.ts
+++ src/server.ts
@@ -7,23 +7,8 @@ import cors from "cors";
 
 setupMongo().then(() => {
   const app = express();
-  
-  //.env e dividir em um array
-  const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [];
 
-  const corsOptions = {
-    origin: (origin: string | undefined, callback: (error: Error | null, allow?: boolean) => void) => {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization'
-  };
-
-  app.use(cors(corsOptions));
+  app.use(cors());
   app.use(express.json());
 
   app.use(json());
